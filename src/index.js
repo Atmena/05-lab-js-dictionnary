bcca18422a5d85beb998605410cfe78dfc98ad37
Atmena <tireau.steph@gmail.com>
2023-06-28 16:54:16 +0200

    chore: Ajout d'un script de défilement d'un menu à droite

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+const rightMenu = document.querySelector("#right-menu");
+const hiddenMenu = document.querySelector("#hidden-menu");
+
+let menuVisible = false;
+
+document.querySelector("#menu-right").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (menuVisible) {
+    hideMenu();
+  } else {
+    showMenu();
+  }
+}
+
+function showMenu() {
+  hiddenMenu.removeAttribute("hidden");
+  hiddenMenu.classList.add("show");
+  rightMenu.style.transform = "translateX(-3.125rem)";
+  menuVisible = true;
+}
+
+function hideMenu() {
+  hiddenMenu.classList.remove("show");
+  rightMenu.style.transform = "translateX(0)";
+  menuVisible = false;
+}
