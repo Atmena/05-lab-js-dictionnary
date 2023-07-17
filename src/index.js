a7149effec8e5775ec9957c77dbb70fc0240bd92
Atmena <tireau.steph@gmail.com>
2023-07-17 16:14:42 +0200

    chore: Mise en place de fontion
    Mise en place d'une fonction pour changer la police d'écriture. Mise en place d'une fonction pour ajouter un Dark Mode

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function searchWord() {
   let word = document.querySelector("#text-input").value;
   let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+  let synApiUrl = "https://api.datamuse.com/words?rel_syn=" + word;
 
   fetch(apiUrl)
     .then(response => response.json())
@@ -53,11 +54,15 @@ function searchWord() {
         if (data[0].phonetics && data[0].phonetics.length > 0 && data[0].phonetics[0].audio) {
           wordElement.innerHTML = `${data[0].word} <img id="prononciation" src="src/media/sound-logo-white.svg" alt="Logo de son" width="20" height="20">`;
           pronunciationIcon = document.querySelector("#prononciation");
-          pronunciationIcon.addEventListener("click", playAudio);
+          if (pronunciationIcon) {
+            pronunciationIcon.addEventListener("click", playAudio);
+          }
         } else {
           wordElement.textContent = data[0].word;
-          pronunciationIcon.style.display = "none";
-          pronunciationIcon.removeEventListener("click", playAudio);
+          if (pronunciationIcon) {
+            pronunciationIcon.style.display = "none";
+            pronunciationIcon.removeEventListener("click", playAudio);
+          }
         }
 
         let definitionElement = document.querySelector("#definition");
@@ -72,6 +77,20 @@ function searchWord() {
         let audioUrl = data[0].phonetics && data[0].phonetics.length > 0 ? data[0].phonetics[0].audio : "";
         let audioElement = document.querySelector("#audio");
         audioElement.src = audioUrl;
+
+        fetch(synApiUrl)
+          .then(response => response.json())
+          .then(synData => {
+            if (synData.length > 0) {
+              let synonymsElement = document.querySelector("#syn-word");
+              let synonyms = synData.map(syn => syn.word);
+              synonymsElement.textContent = synonyms.join(", ");
+            } else {
+              let synonymsElement = document.querySelector("#syn-word");
+              synonymsElement.textContent = "No synonyms found.";
+            }
+          })
+          .catch(error => console.log("An error occurred while fetching synonyms: ", error));
       } else {
         console.log("No results found for the searched word.");
       }
@@ -82,4 +101,39 @@ function searchWord() {
 function playAudio() {
   let audioElement = document.querySelector("#audio");
   audioElement.play();
+}
+
+let fontOptions = document.querySelectorAll(".font-option");
+
+fontOptions.forEach(option => {
+  option.addEventListener("click", changeFont);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  let siteBody = document.querySelector("body");
+  siteBody.classList.add("serif");
+});
+
+function changeFont(event) {
+  let fontClass = event.target.classList[1];
+  let siteBody = document.querySelector("body");
+  
+  siteBody.classList.remove("serif", "sans-serif", "monospace");
+
+  siteBody.classList.add(fontClass);
+
+  fontOptions.forEach(option => {
+    option.classList.remove("selected");
+  });
+
+  event.target.classList.add("selected");
+}
+
+const modeToggle = document.querySelector(".color-selector");
+const siteBody = document.querySelector("body");
+
+modeToggle.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() {
+  siteBody.classList.toggle("dark-mode");
 }
